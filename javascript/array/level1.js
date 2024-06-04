992e91bbf406bcc8eb485a3aff48d12311d8ee2f
angel_tamang <tamangangel2057@gmail.com>
2024-06-04 17:13:04 +0545

    arraylevel1

---
+++ javascript/array/level1.js
@@ -0,0 +1,19 @@
+//Q1 find list of numbers divisible by 3
+
+const arr = [3,5,1,2,9,12]
+//write your code here
+
+console.log(arr.filter((item)=>{if (item %3 === 0) {
+    return item;
+}}))
+
+//expected output---> [3,9,12]
+// hint: %3 === 0
+// filter
+//syntax:
+// arr.filter((item,id)=>{
+//     //
+// })
+
+
+//----------------
